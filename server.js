328e2f788f53fe7a309b126c636fd7fde29bb79b
LOVERLIKEME <107211931+LOVERLIKEME@users.noreply.github.com>
2022-09-11 21:12:46 +0900

    테스트

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const socket = require('socket.io');
 const fs = require('fs');
 const app = express();
-var PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 const server = app.listen(PORT);
 const cors = require ('cors');
 
